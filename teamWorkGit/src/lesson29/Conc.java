9436e086bf54a4f500b635410c4e3849945f60ff
Leonid221 <leonid.natochiy@gmail.com>
2022-06-16 13:27:29 +0300

    Commit cocnmethod

---
+++ teamWorkGit/src/lesson29/Conc.java
@@ -0,0 +1,12 @@
+package lesson29;
+
+public class Conc {
+
+    public static String concatStr(String a, String b){
+        return a +" "+ b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(concatStr("my","car"));
+    }
+}
