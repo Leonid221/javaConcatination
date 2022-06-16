a5bf76eed4cbce03107dea951ff1a9d3c96a4140
Александра <aleksandra@MacBook-Pro-Andrey.local>
2022-06-16 23:30:08 +0300

    String size function

--- teamWorkGit/src/lesson29/Conc.java
+++ teamWorkGit/src/lesson29/Conc.java
@@ -1,12 +1,16 @@
 package lesson29;
 
 public class Conc {
+    public static String concatStr(String a, String b) {
+        return a + " " + b;
+    }
 
-    public static String concatStr(String a, String b){
-        return a +" "+ b;
+    public static int strSize(String str) {
+        return str.length();
     }
 
     public static void main(String[] args) {
-        System.out.println(concatStr("my","car"));
+        System.out.println(concatStr("my", "car"));
+        System.out.println(strSize("my-new-car"));
     }
 }
